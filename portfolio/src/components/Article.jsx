26f6bf270133990220c085227299e1487238abcd
Jack Bittner <jack.bittner@outlook.com>
2024-02-28 21:11:24 -0600

    finishing article template

--- portfolio/src/components/Article.jsx
+++ portfolio/src/components/Article.jsx
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from 'react';
 import article from "../components/layouts/aboutMe.json";
-
 const Article = () => {
-
   const title = article[0].title;
   const imgPath = article[0].image;
   const imgLabel = article[0].imageLabel;
   const imgStats = article[0].imageStats;
   const statsEmpty = !imgStats || Object.keys(imgStats).length === 0;
-  const textBody = article[1].articleBody;
 
   console.log(article)
 
@@ -35,6 +32,24 @@ const Article = () => {
     return <li>{items}</li>;
   };
 
+  const renderArticleBody = (text) => {
+    if (typeof text === 'string' && text.includes('\n')) {
+      return text.split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+          {line}
+          <br />
+        </React.Fragment>
+      ));
+    }
+    return <p>{text}</p>;
+  };
+
+  const renderArticleImage = (imagePath) => (
+    <div className="img-box p-2 float-start">
+      <img src={`./src/assets/${imagePath}`} alt="Article Image" />
+    </div>
+  );
+
   return (
     <>
       <div className="container">
@@ -42,7 +57,13 @@ const Article = () => {
         <div className="row">
           <div className="col-7 d-flex flex-column align-items-center py-2">
             <hr width="90%"/>
-            <p className="py-1 article-body">{renderStatItems(textBody)}</p>
+            {article.slice(1).map((item, index) => (
+              <React.Fragment key={index}>
+                {item.articleBody && (
+                  <p className="py-1 article-body">{item.articleImage && renderArticleImage(item.articleImage)}{renderArticleBody(item.articleBody)}</p>
+                )}
+              </React.Fragment>
+            ))}
           </div>
           <div className="col-5 d-flex flex-column align-items-center stats-box py-2">
             <h2 className="w-100">{imgLabel}</h2>
@@ -53,7 +74,7 @@ const Article = () => {
               Object.entries(imgStats).map(([key, value]) => (
                 <div key={key} className="row stat-item w-100">
                   <div className="col-4">
-                    <p class="text-end">
+                    <p className="text-end">
                       <strong>{key}</strong>
                     </p>
                   </div>
